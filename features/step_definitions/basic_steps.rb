70856a7f0c0efb852313023934f37f4a098175d0
Joe Torreggiani <joetorreggiani@gmail.com>
2019-03-02 16:03:58 -0500

    Add basic admin user feature specification

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -2,6 +2,10 @@ Given(/^I am on the home page$/) do
   visit('/')
 end
 
+Given(/^I am on the admin login page$/) do
+  visit('/admin/login')
+end
+
 Then(/^I should see the text "(.*?)"$/) do |text|
   assert_match(/#{text}/i, page.text)
 end
@@ -12,4 +16,8 @@ end
 
 When(/^I click the "(.*?)" button$/) do |selector|
   click_button(selector)
+end
+
+Given(/^the user "(.*?)" exists$/) do |name|
+  create_user(name)
 end
