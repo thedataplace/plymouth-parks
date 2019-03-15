9984eb9bd8503b1ddd8b6e757546c7d88806e3ee
Joe Torreggiani <joetorreggiani@gmail.com>
2019-03-15 22:03:32 +0000

    Handle missing images in admin panel

--- app/admin/data_entries.rb
+++ app/admin/data_entries.rb
@@ -2,18 +2,26 @@
 ActiveAdmin.register DataEntry do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  permit_params :title, :subtitle, :username, :latitude, :longitude, :notes, :image
+  permit_params :title,
+                :subtitle,
+                :username,
+                :latitude,
+                :longitude,
+                :notes,
+                :image
+
   index do
     selectable_column
     id_column
     column :title
     column :subtitle
-    column :subtitle
     column :latitude
     column :longitude
     column :created_at
     column :image do |entry|
-      image_tag url_for(entry.image), width: '50'
+      if entry.image.attached?
+        image_tag url_for(entry.image), width: '50'
+      end
     end
     actions
   end
@@ -21,18 +29,18 @@ ActiveAdmin.register DataEntry do
     attributes_table do
       row :title
       row :subtitle
-      row :subtitle
       row :latitude
       row :longitude
       row :image do |entry|
-        image_tag url_for(entry.image), width: '500'
+        if entry.image.attached?
+          image_tag url_for(entry.image), width: '500'
+        end
       end
     end
   end
   form do |f|
     f.inputs do
       f.input :title, as: :string
-      f.input :subtitle, as: :string
       f.input :subtitle, as: :string
       f.input :latitude, as: :number, step: '0.000001'
       f.input :longitude, as: :number, step: '0.000001'
