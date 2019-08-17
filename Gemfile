c154897ba76bd6563020659755572f85af449145
Joe Torreggiani <joetorreggiani@gmail.com>
2019-08-17 13:01:03 +0100

    Fix linter issues and add tests

--- Gemfile
+++ Gemfile
@@ -72,6 +72,8 @@ group :test do
   # Adds support for Capybara system testing and selenium driver
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
+
+  gem 'minitest-stub_any_instance'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/models/data_entry.rb
+++ app/models/data_entry.rb
@@ -3,25 +3,33 @@ class DataEntry < ApplicationRecord
   has_one_attached :image
   # has_one_attached :secondary_image
 
-  after_create :save_image_storage_url!
+  after_save :save_image_storage_url!
+
+  scope :with_image_urls_near_expiry, lambda {
+    where('image_storage_url_expiry_date <= ?', expiry_date)
+  }
+
+  def self.expiry_date
+    Date.today + 6.days
+  end
+
+  def self.refresh_stale_image_storage_urls
+    with_image_urls_near_expiry.each(&:save_image_storage_url!)
+  end
 
   def image_url
+    return unless image.attached?
+
     image.variant(auto_orient: true).processed.service_url
   end
 
   def save_image_storage_url!
-    if image.attached?
-      update_attributes(image_storage_url: image.service_url,
-                        image_storage_url_expiry_date: Date.today + 6.days)
+    if image_url
+      update_columns(image_storage_url: image_url,
+                     image_storage_url_expiry_date: DataEntry.expiry_date)
     else
-      update_attributes(image_storage_url: nil,
-                        image_storage_url_expiry_date: nil)
+      update_columns(image_storage_url: nil,
+                     image_storage_url_expiry_date: nil)
     end
-  end
-
-  def self.refresh_stale_image_storage_urls
-    target_date = Date.today + 6.days
-    entries = where("image_storage_url_expiry_date <= ?", target_date)
-    entries.each { |entry| entry.save_image_storage_url! }
   end
 end
