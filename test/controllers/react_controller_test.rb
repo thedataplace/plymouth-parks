2c206c2f9ab71d8b4e38e8bc9797c2166397081a
Joe Torreggiani <joetorreggiani@gmail.com>
2019-09-01 14:24:32 +0100

    Fix spec

--- test/controllers/react_controller_test.rb
+++ test/controllers/react_controller_test.rb
@@ -2,8 +2,7 @@ require 'test_helper'
 
 class ReactControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get react_index_url
+    get trees_url
     assert_response :success
   end
-
 end
