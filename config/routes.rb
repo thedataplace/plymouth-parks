963a52b88e670e2031df89acdb683111cf6a6f18
Joe Torreggiani <joetorreggiani@gmail.com>
2020-11-09 07:54:30 -0500

    Add user api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :data_entries
+      resources :users, only: [:index]
       devise_scope :user do
         post '/tokens', to: 'tokens#create'
         post '/register', to: 'registrations#create'
